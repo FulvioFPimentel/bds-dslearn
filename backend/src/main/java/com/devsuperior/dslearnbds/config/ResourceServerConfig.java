9d9f81d6f566aaac84f52c8d586e5691c1039437
FulvioFPimentel <fulviofp@hotmail.com>
2021-05-17 11:53:30 -0300

    Oauth security, login

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
@@ -0,0 +1,76 @@
+package com.devsuperior.dslearnbds.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.anyRequest().authenticated();
+		
+		http.cors().configurationSource(corsConfigurationSource());
+		
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();                                    // Configuração do cors 
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));								   // Quem vai poder acessar (“*”)
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));	   // Quais métodos vou permitir ("POST", "GET", "PUT", "DELETE", "PATCH")
+		corsConfig.setAllowCredentials(true);													   // Vou permitir Credencial (true)
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));			   // Quais cabeçalho eu vou deixar 
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);									    //Todos os caminhos vão pelas configurações citado acima 
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean 
+			= new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);													// Registra o Cors com a máxima precedência 
+		return bean;
+	}	
+	
+	
+}
